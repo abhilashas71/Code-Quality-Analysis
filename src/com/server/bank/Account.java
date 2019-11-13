d16a334388322f0dd567ac0a3034ef9165f62f27
Abhilasha <abhilashas71@gmail.com>
2019-11-13 11:42:14 +0530

    Just like that

--- src/com/server/bank/Account.java
+++ src/com/server/bank/Account.java
@@ -4,7 +4,7 @@ public class Account {
 	private String branchName;
 	public Account(){
 		System.out.println("Account.Account() here.");
-		System.out.println("just like that");
+		//System.out.println("just like that");
 	}
 	
 	public Account(String newName)
